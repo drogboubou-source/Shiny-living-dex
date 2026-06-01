9b4afb77385dc4d7788a57183f7ab30f85ab6120
drogboubou-source <drogboubou@gmail.com>
2026-06-01 19:59:10 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,42 +1,31 @@
-const CACHE_NAME = "shiny-dex-v1";
-const ASSETS = [
-    "./",
-    "./index.html",
-    "./style.css",
-    "./app.js",
-    "./manifest.json"
+const CACHE_NAME = "shiny-living-dex-v1";
+
+const urlsToCache = [
+    "/",
+    "/index.html",
+    "/css/style.css",
+    "/js/app.js",
+    "/manifest.json"
 ];
 
-// Installation : Mise en cache des fichiers essentiels
-self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS);
-        })
+self.addEventListener("install", event => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+
     );
-    self.skipWaiting();
+
 });
 
-// Activation et nettoyage des anciens caches
-self.addEventListener("activate", (e) => {
-    e.waitUntil(
-        caches.keys().then((keys) => {
-            return Promise.all(
-                keys.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                })
-            );
-        })
-    );
-});
+self.addEventListener("fetch", event => {
 
-// Stratégie Réseau puis Cache (permet de voir les modifs si connecté)
-self.addEventListener("fetch", (e) => {
-    e.respondWith(
-        fetch(e.request).catch(() => {
-            return caches.match(e.request);
-        })
+    event.respondWith(
+
+        caches.match(event.request)
+        .then(response => response || fetch(event.request))
+
     );
+
 });
