52781db51f5fb53eae12469432eb854a6b5953cb
drogboubou-source <drogboubou@gmail.com>
2026-07-14 08:33:48 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'shiny-living-dex-v273';
-const RUNTIME_CACHE = 'shiny-living-dex-runtime-v273';
+const CACHE_NAME = 'shiny-living-dex-v274';
+const RUNTIME_CACHE = 'shiny-living-dex-runtime-v274';
 const OFFLINE_URL = './index.html';
 const APP_SHELL = [
   './',
